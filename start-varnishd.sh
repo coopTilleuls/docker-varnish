147d1ea1ca47a6ac58d7d018bd4ab3b02f3433e2
Albert Sun <albert@albertsun.info>
2016-02-22 16:54:16 -0500

    support unusually large headers

--- start-varnishd.sh
+++ start-varnishd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec varnishd -j unix,user=varnishd -F -f /etc/varnish/default.vcl -s malloc,${VARNISH_MEMORY} -a 0.0.0.0:${VARNISH_PORT}
+exec varnishd -j unix,user=varnishd -F -f /etc/varnish/default.vcl -s malloc,${VARNISH_MEMORY} -a 0.0.0.0:${VARNISH_PORT} -p http_req_hdr_len=16384 -p http_resp_hdr_len=16384
